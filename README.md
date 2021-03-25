437413c76457c501334e1ab651607d766c3bbe17
Erik Gillespie <erik.gillespie@gmail.com>
2021-03-25 17:11:15 -0400

    Switch to Firestore

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Joke-a-tron 9001
 
-A database of jokes, backed by Firebase!
+A database of jokes, backed by Firestore!
 
 To see the site in action, visit [joke-a-tron.surge.sh](https://joke-a-tron.surge.sh/).
--- app.js
+++ app.js
@@ -1,119 +1,109 @@
-// Initialize Firebase
-var config = {
-  apiKey: 'AIzaSyD0ad33Xon8PkZqbtm5mBVTRlX-ONT-Nzg',
-  authDomain: 'joke-a-tron-9000.firebaseapp.com',
-  databaseURL: 'https://joke-a-tron-9000.firebaseio.com',
-  projectId: 'joke-a-tron-9000',
-  storageBucket: 'joke-a-tron-9000.appspot.com',
-  messagingSenderId: '87962746854'
+/* global firebase */
+const firebaseConfig = {
+  apiKey: 'AIzaSyCKj7jge9Vq__aZN9CEdsiWwgTYjW2GOTk',
+  authDomain: 'joke-a-tron-9000-99123.firebaseapp.com',
+  projectId: 'joke-a-tron-9000-99123',
+  storageBucket: 'joke-a-tron-9000-99123.appspot.com',
+  messagingSenderId: '842807512042',
+  appId: '1:842807512042:web:26a94beca6acd9970c4093',
+  measurementId: 'G-C4XDW252BW'
 }
-firebase.initializeApp(config)
-firebase.auth().signInAnonymously()
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+const db = firebase.firestore()
 
 // ----
 // DATA
 // ----
 
-// The message to display if the jokes object is empty
-var noJokesMessage = 'I... I don\'t know any jokes. 😢'
+// A couple jokes to start with
+const jokes = {}
 
-// HTML elements
-var requestedJokeInput = document.getElementById('requested-joke')
-var jokeBox = document.getElementById('joke-box')
-var jokesMenuList = document.getElementById('jokes-menu')
+// The message to display if the jokes object is empty
+const noJokesMessage = 'I... I don\'t know any jokes. 😢'
 
 // -------------
 // PAGE UPDATERS
 // -------------
 
-// Add the joke to the list
-var jokeAdded = function (jokeSnapshot) {
-  jokesMenuList.innerHTML += '<li>' + jokeSnapshot.key + '</li>'
-}
-
-// Set the displayed joke
-var jokeChanged = function (jokeSnapshot) {
-  setDisplayedJoke(jokeSnapshot.key)
-}
-
-// Remove the joke from the list
-var jokeRemoved = function (jokeSnapshot) {
-  var listItems = document.getElementsByTagName('li')
-  for (var i = 0; i < listItems.length; i++) {
-    if (listItems[i].textContent === jokeSnapshot.key) {
-      listItems[i].parentElement.removeChild(listItems[i])
-      if (requestedJokeInput.value === jokeSnapshot.key) {
-        setDisplayedJoke('')
-      }
-      break
-    }
-  }
-}
-
 // Update the listed jokes, based on the jokes object
-var updateJokesMenu = function () {
+const jokesMenuList = document.getElementById('jokes-menu')
+const updateJokesMenu = function () {
   // Don't worry too much about this code for now.
   // You'll learn how to do advanced stuff like
   // this in a later lesson.
-  var jokeKeys = Object.keys(jokes)
-  var jokeKeyListItems = jokeKeys.join('</li><li>') || noJokesMessage
+  const jokeKeys = Object.keys(jokes)
+  const jokeKeyListItems = jokeKeys.join('</li><li>') || noJokesMessage
   jokesMenuList.innerHTML = '<li>' + jokeKeyListItems + '</li>'
 }
 
-// Set displayed joke to a specific value
-var setDisplayedJoke = function (jokeKey) {
-  requestedJokeInput.value = jokeKey
-  updateDisplayedJoke()
-}
-
 // Update the displayed joke, based on the requested joke
-var updateDisplayedJoke = function () {
-  var requestedJokeKey = requestedJokeInput.value.trim()
-
-  if (requestedJokeKey) {
-    firebase.database().ref('jokes').child(requestedJokeKey).once('value')
-    .then(function (jokeSnapshot) {
-      if (jokeSnapshot.exists()) {
-        var joke = jokeSnapshot.val()
-        jokeBox.innerHTML = '<p>' + joke.setup + '</p><p>' + joke.punchline + '</p'
-      } else {
-        jokeBox.textContent = noJokesMessage
-      }
-    })
+const requestedJokeInput = document.getElementById('requested-joke')
+const jokeBox = document.getElementById('joke-box')
+const updateDisplayedJoke = function () {
+  const requestedJokeKey = requestedJokeInput.value
+  if (jokes[requestedJokeKey]) {
+    const joke = jokes[requestedJokeKey]
+    jokeBox.innerHTML = '<p>' + joke.setup + '</p><p>' + joke.punchline + '</p>'
   } else {
-    jokeBox.textContent = noJokesMessage
+    jokeBox.textContent = 'I don\'t know that joke...'
   }
 }
 
-// Remember a joke
-var rememberJoke = function () {
-  var newJokeKey = document.getElementById('newJokeKey')
-  var newJokeSetup = document.getElementById('newJokeSetup')
-  var newJokePunchline = document.getElementById('newJokePunchline')
-
-  firebase.database().ref('jokes').child(newJokeKey.value).set({
-    setup: newJokeSetup.value,
-    punchline: newJokePunchline.value
-  })
-
-  newJokeKey.value = ''
-  newJokeSetup.value = ''
-  newJokePunchline.value = ''
-  setDisplayedJoke('')
+// Add a new joke
+const rememberJokeKey = document.getElementById('remember-joke-key')
+const rememberJokeSetup = document.getElementById('remember-joke-setup')
+const rememberJokePunchline = document.getElementById('remember-joke-punchline')
+const rememberJokeButton = document.getElementById('remember-joke-button')
+const rememberJoke = function () {
+  const key = rememberJokeKey.value
+  const setup = rememberJokeSetup.value
+  const punchline = rememberJokePunchline.value
+  if (key.trim().length > 0) {
+    db.collection('jokes').doc(key).set({
+      setup: setup,
+      punchline: punchline
+    })
+  }
 }
 
-// Forget a joke
-var forgetJoke = function () {
-  var badJokeKey = document.getElementById('badJokeKey')
-  firebase.database().ref('jokes').child(badJokeKey.value).remove()
-  badJokeKey.value = ''
+// Delete a joke
+const forgetJokeKey = document.getElementById('forget-joke-key')
+const forgetJokeButton = document.getElementById('forget-joke-button')
+const forgetJoke = function () {
+  const key = forgetJokeKey.value
+  if (key.length > 0) {
+    db.collection('jokes').doc(key).delete()
+  }
+}
+
+// Function to keep track of all other
+// page update functions, so that we
+// can call them all at once
+const updatePage = function () {
+  updateJokesMenu()
+  updateDisplayedJoke()
 }
 
 // -------
 // STARTUP
 // -------
 
-updateDisplayedJoke()
+db.collection('jokes')
+  .onSnapshot(function (snapshot) {
+    snapshot.docChanges().forEach(function (change) {
+      const joke = change.doc.data()
+      if (change.type === 'added') {
+        jokes[change.doc.id] = joke
+      } else if (change.type === 'modified') {
+        jokes[change.doc.id] = joke
+      } else if (change.type === 'removed') {
+        delete jokes[change.doc.id]
+      }
+    })
+    updatePage()
+  })
 
 // ---------------
 // EVENT LISTENERS
@@ -121,9 +111,5 @@ updateDisplayedJoke()
 
 // Keep the requested joke up-to-date
 requestedJokeInput.addEventListener('input', updateDisplayedJoke)
-document.getElementById('remember').addEventListener('click', rememberJoke)
-document.getElementById('forget').addEventListener('click', forgetJoke)
-
-firebase.database().ref('jokes').on('child_added', jokeAdded)
-firebase.database().ref('jokes').on('child_changed', jokeChanged)
-firebase.database().ref('jokes').on('child_removed', jokeRemoved)
+rememberJokeButton.addEventListener('click', rememberJoke)
+forgetJokeButton.addEventListener('click', forgetJoke)
